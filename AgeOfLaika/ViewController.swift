792b473f152701676b1336cbb16959e31f5eda76
Robert E Spivack <spiv@spivtech.com>
2014-11-06 20:20:12 -0800

    Finished

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    
+    
+    @IBOutlet weak var convertedDogYearsLabel: UILabel!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +30,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func convertToDogYears(sender: UIButton) {
+        
+        let humanAge = enterHumanYearsTextField.text.toInt()!
+        let conversionConstant = 7
+        
+        convertedDogYearsLabel.text = enterHumanYearsTextField.text + " Human years is " +
+            "\(humanAge * conversionConstant)" + " in Dog years"
+        
+        enterHumanYearsTextField.text = ""
+        enterHumanYearsTextField.resignFirstResponder()
+        convertedDogYearsLabel.hidden = false
+    }
+    
+    @IBAction func convertToRealDogYearsButton(sender: UIButton) {
+        
+        let humanAge = Double((enterHumanYearsTextField.text as NSString).doubleValue)
+        var realDogYears = 0.0
 
+        if humanAge <= 2.0 {
+            realDogYears = humanAge * 10.5
+        }
+        else {
+            realDogYears = (2.0 * 10.5) + ((humanAge - 2.0) * 4)
+        }
+        
+        
+        convertedDogYearsLabel.text = enterHumanYearsTextField.text + " Human years is " +
+            "\(realDogYears)" + " in Dog years"
+        
+        enterHumanYearsTextField.text = ""
+        enterHumanYearsTextField.resignFirstResponder()
+        convertedDogYearsLabel.hidden = false
+        
+        
+    }
 }
 
